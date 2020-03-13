4466720d1c57f745eb402aa78a83075c181ddf01
Mohammed Ezzedine <mhmd.ezdn@gmail.com>
2020-03-13 18:15:14 +0200

    Adding `Back to List` Navigation Links

--- Outlook/backend/Models/Issue.cs
+++ Outlook/backend/Models/Issue.cs
@@ -10,9 +10,13 @@ namespace backend.Models
         public int Id { get; set; }
         [DisplayName("Issue Number")]
         public int IssueNumber { get; set; }
+        [DisplayName("Arabic PDF")]
         public string ar_pdf { get; set; }
+        [DisplayName("English PDF")]
         public string en_pdf { get; set; }
+        [DisplayName("Arabic Cover")]
         public string ar_cover { get; set; }
+        [DisplayName("English Cover")]
         public string en_cover { get; set; }
         public int VolumeID { get; set; }
         [NotMapped]
