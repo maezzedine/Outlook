a69038e5b5a117cb52245148833490b1190565c0
Mohammed Ezzedine <mhmd.ezdn@gmail.com>
2020-03-17 14:45:38 +0200

    Issues API Added

---
+++ Outlook/backend/APIs/IssuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Models;
+
+namespace backend.APIs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssuesController : ControllerBase
+    {
+        private readonly OutlookContext _context;
+
+        public IssuesController(OutlookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Issues
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Issue>>> GetIssue()
+        {
+            return await _context.Issue.ToListAsync();
+        }
+
+        // GET: api/Issues/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Issue>> GetIssue(int id)
+        {
+            var issue = await _context.Issue.FindAsync(id);
+
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            return issue;
+        }
+    }
+}
