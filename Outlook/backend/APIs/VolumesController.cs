817f5a73aa915ce69db1937f6cb782018ef6fdf4
Mohammed Ezzedine <mhmd.ezdn@gmail.com>
2020-03-17 14:31:02 +0200

    Add Volume API

---
+++ Outlook/backend/APIs/VolumesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Models;
+
+namespace backend.APIs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VolumesController : ControllerBase
+    {
+        private readonly OutlookContext context;
+
+        public VolumesController(OutlookContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Volumes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Volume>>> GetVolume()
+        {
+            return await context.Volume.ToListAsync();
+        }
+    }
+}
