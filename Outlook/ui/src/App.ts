fae91177da40ffeafc6247fbc17742b3e3fd6203
Mohammed Ezzedine <mhmd.ezdn@gmail.com>
2020-03-22 15:15:34 +0200

    CSS-Theming Added

--- Outlook/ui/src/App.ts
+++ Outlook/ui/src/App.ts
@@ -1,6 +1,15 @@
 import { Component, Vue } from 'vue-property-decorator';
+import Home from './views/Home/Home.vue';
+import { initializeTheming } from 'css-theming';
 
-@Component
+@Component({
+    components: {
+        Home
+    }
+    
+})
 export default class extends Vue {
-
+    created() {
+        initializeTheming();
+    }
 }
---
+++ Outlook/ui/src/views/Home/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        Home
+    </div>
+</template>
+
+<script lang="ts" src="./Home.ts"></script>
+<style lang="scss" src="./Home.scss"></style>
