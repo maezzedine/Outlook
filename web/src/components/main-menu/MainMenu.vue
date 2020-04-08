34ca55b8a3ce62dca7ee1d9dad3046bbbe850880
Mohammed <mezdn@outlook.com>
2020-04-08 15:31:38 +0300

    Add article stats

--- web/src/components/main-menu/MainMenu.vue
+++ web/src/components/main-menu/MainMenu.vue
@@ -7,6 +7,14 @@
                 <div class="subtitle"><span>{{article.writer}}</span> {{article.subtitle}}</div>
 
                 <img v-if="article.picturePath != null" :src="APP_URL + article.picturePath" />
+                <div class="footer">
+                    <span :class="[{ ratedUp : article.ratedByUser == 1 }]"><i class="fas fa-angle-up"></i></span>
+                    {{article.rate}}
+                    <span :class="[{ ratedDown : article.ratedByUser == 2 }]"><i class="fas fa-angle-down"></i></span>
+                    <span>{{Language.votes}}: {{article.numberOfVotes}}</span>
+                    <span>{{Language.comments}}: {{article.comments.length}}</span>
+                    <span>{{Language.favorites}}: {{article.numberOfFavorites}}</span>
+                </div>
             </template>
         </div>
     </div>
